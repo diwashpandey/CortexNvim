48a58c4841b2c851609f80d5ff6e68de9e85d65d
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2025-09-27 09:51:53 +0545

    Installed Dressing for better ui

---
+++ lua/diwash/plugins/dressing.lua
@@ -0,0 +1,7 @@
+-- This plugin improves Neovim UI for prompts and input windows
+-- Example: better `:ls` input, nicer `vim.ui.select()` menus, rename, copy, paste, etc.
+
+return {
+  'stevearc/dressing.nvim',
+  event = "VeryLazy",
+}
