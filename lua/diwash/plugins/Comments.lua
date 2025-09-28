d797dd758167f4e50b3a8ca682a706af4725e844
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2025-09-28 09:58:35 +0545

    Installed Comments.lua for easy commenting

---
+++ lua/diwash/plugins/Comments.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "numToStr/Comment.nvim",
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring"
+    },
+    config = function()
+      local ts_comment = require("ts_context_commentstring.integrations.comment_nvim")
+
+      require("Comment").setup({
+        pre_hook = ts_comment.create_pre_hook(), -- Treesitter-aware comments
+      })
+    end,
+  },
+}
