122e82d1e732d876328a875487c14d70ebea7fe6
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2025-09-28 09:58:56 +0545

    Updated bufferline config for shortcuts

--- lua/diwash/plugins/bufferline.lua
+++ lua/diwash/plugins/bufferline.lua
@@ -11,10 +11,23 @@ return {
             }
 
             -- make bufferline transparent
-            vim.api.nvim_set_hl(0, "BufferLineFill",       {bg = "NONE"})
+            vim.api.nvim_set_hl(0, "BufferLineFill", {bg = "NONE"})
             vim.api.nvim_set_hl(0, "BufferLineBackground", {bg = "NONE"})
-            vim.api.nvim_set_hl(0, "BufferLineSeparator",  {bg = "NONE"})
+            vim.api.nvim_set_hl(0, "BufferLineSeparator", {bg = "NONE"})
             vim.api.nvim_set_hl(0, "BufferLineSeparatorSelected", {bg = "NONE"})
             vim.api.nvim_set_hl(0, "BufferLineIndicatorSelected", {bg = "NONE"})
+
+            -- Navigations
+
+            vim.keymap.set('n', '<leader>b[', '<cmd>BufferLineCyclePrev<CR>') -- Go to previous buffer
+            vim.keymap.set('n', '<leader>b]', '<cmd>BufferLineCycleNext<CR>') -- Go to next buffer
+
+            -- Jump to buffer number 1-9
+            for i = 1, 9 do
+              vim.keymap.set('n', '<leader>b' .. i, '<cmd>BufferLineGoToBuffer ' .. i .. '<CR>')
+            end
+
+            -- Delete/close current buffer
+            vim.keymap.set('n', '<leader>bx', '<cmd>bdelete<CR>') -- bdelete is nvim ommand
           end
 }
