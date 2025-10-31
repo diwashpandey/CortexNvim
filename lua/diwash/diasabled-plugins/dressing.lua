954b2909bf677045e492d097b069aac07aac8347
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2025-10-31 07:30:25 +0545

    Removing Unused/Disabled Plugins

--- lua/diwash/diasabled-plugins/dressing.lua
+++
@@ -1,8 +0,0 @@
--- This plugin improves Neovim UI for prompts and input windows
--- Example: better `:ls` input, nicer `vim.ui.select()` menus, rename, copy, paste, etc.
-
-return {
-  'stevearc/dressing.nvim',
-  event = "VeryLazy",
-  opts = {}
-}
