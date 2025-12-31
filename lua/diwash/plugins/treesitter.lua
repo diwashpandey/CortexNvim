695b7e061526b067aaaf17c7c260a2fc9d573d6d
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2025-12-31 12:08:23 +0545

    Updated Tree-sitter config to support latest update

--- lua/diwash/plugins/treesitter.lua
+++ lua/diwash/plugins/treesitter.lua
@@ -1,63 +1,24 @@
--- TODO: Diwash
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPost", "BufNewFile" },
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "python",
-        "javascript",
-        "typescript",
-        "tsx",
-        "go",
-        "html",
-        "css",
-        "scss",
-        "json",
-        "yaml",
-        "bash",
-        "lua",
-      },
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<CR>",       -- Press enter to start selection
-          node_incremental = "<CR>",     -- Expand selection
-          node_decremental = "<BS>",     -- Shrink selection
-          scope_incremental = "<TAB>",   -- Select entire scope
-        },
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["af"] = "@function.outer", -- Whole function
-            ["if"] = "@function.inner", -- Inside function
-            ["ac"] = "@class.outer",    -- Whole class
-            ["ic"] = "@class.inner",    -- Inside class
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            ["]m"] = "@function.outer",
-            ["]]"] = "@class.outer",
-          },
-          goto_previous_start = {
-            ["[m"] = "@function.outer",
-            ["[["] = "@class.outer",
-          },
-        },
-      },
+    -- Setup the new minimal Treesitter (no old configs)
+    require("nvim-treesitter").setup {
+      -- optional parser install dir, adjust if needed
+      install_dir = vim.fn.stdpath("data") .. "/site",
+    }
+
+    -- Manually install languages you want (can be async)
+    require("nvim-treesitter").install { "python", "javascript", "typescript", "go", "lua", "bash", "html", "css", "json", "yaml" }
+
+    -- Enable highlighting using Neovim built-in treesitter start
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "*",
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
     })
   end,
 }
