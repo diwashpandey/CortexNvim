35049989ec131b15082fbddb3abb9f14692d3a73
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2026-07-29 13:23:56 +0545

    Updated Bindings

--- lua/bindings.lua
+++ lua/bindings.lua
@@ -9,30 +9,23 @@ map("i", "jj", "<Esc>", { noremap = true, silent = true })
 
 
 ----------------
---- Windows ----
+--- Panes (Windows) ----
 ----------------
-map("n", "<leader>wv", "<C-w>v", { desc = "Vertical split" })
-map("n", "<leader>wh", "<C-w>s", { desc = "Horizontal split" })
-map("n", "<leader>wq", "<C-w>q", { desc = "Close window" })
+map("n", "<leader>ps", "<C-w>v", { desc = "Vertical split" })
+map("n", "<leader>ph", "<C-w>s", { desc = "Horizontal split" })
+map("n", "<leader>pq", "<C-w>q", { desc = "Close window" })
 
-vim.keymap.set("n", "<leader>wh", "<C-w>h", { desc = "Go to left window" })
-vim.keymap.set("n", "<leader>wj", "<C-w>j", { desc = "Go to lower window" })
-vim.keymap.set("n", "<leader>wk", "<C-w>k", { desc = "Go to upper window" })
-vim.keymap.set("n", "<leader>wl", "<C-w>l", { desc = "Go to right window" })
-
-vim.keymap.set("n", "<leader>h", "<C-w>h", { desc = "Move to left split" })
-vim.keymap.set("n", "<leader>j", "<C-w>j", { desc = "Move to lower split" })
-vim.keymap.set("n", "<leader>k", "<C-w>k", { desc = "Move to upper split" })
-vim.keymap.set("n", "<leader>l", "<C-w>l", { desc = "Move to right split" })
+map("n", "<leader>h", "<C-w>h", { desc = "Move to left split" })
+map("n", "<leader>j", "<C-w>j", { desc = "Move to lower split" })
+map("n", "<leader>k", "<C-w>k", { desc = "Move to upper split" })
+map("n", "<leader>l", "<C-w>l", { desc = "Move to right split" })
 
 ----------------
---- Buffers ----
+--- Tabs(Buffers) ----
 ----------------
-map("n", "<leader>bn", "<cmd>bnext<cr>", { desc = "Next buffer" })
-map("n", "<leader>bp", "<cmd>bprev<cr>", { desc = "Prev buffer" })
-map("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete buffer" })
-map("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
-map("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Next buffer" })
+map("n", "<leader>tn", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map("n", "<leader>tp", "<cmd>bprev<cr>", { desc = "Prev buffer" })
+map("n", "<leader>tq", "<cmd>bdelete<cr>", { desc = "Quit buffer" })
 
 -------------------
 --- Move Lines ----
